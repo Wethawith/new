e63c7fd3c2277d94f11c30772425dbe99b4d8bec
Wethawith <ikkyu928@gmail.com>
2025-02-06 14:45:13 +0700

    frist

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 // Import your components/pages
 import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import Room from './pages/Room';
+import Booking from './pages/Booking';
 
 function App() {
   return (
     <Router>
       <div>
         {/* Navigation Menu */}
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg bg-body-pimary">
           <div class="container-fluid">
-            <a class="navbar-brand" href="/">Menu</a>
+            <a class="navbar-brand" href="/">Porn Hotel</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
@@ -24,10 +24,10 @@ function App() {
                   <a class="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="About">About</a>
+                  <a class="nav-link" href="Room">Room</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="Contact">Contact</a>
+                  <a class="nav-link" href="Booking">Booking</a>
                 </li>
               </ul>
             </div>
@@ -37,8 +37,8 @@ function App() {
         {/* Define Routes */}
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/Room" element={<Room />} />
+          <Route path="/Booking" element={<Booking />} />
         </Routes>
       </div>
     </Router>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,53 @@
 import React from "react";
+import '../style/home_st.css';
+import room1 from '../img/r1.jpg';
+import room2 from '../img/r2.jpg';
+import room3 from '../img/r3.jpg';
 
 function Home () {
     return (
         <div className="Home">
-
+            <nav class="navbar">
+                <div class="container">
+                    <a class="navbar-brand" href="#"></a>
+                </div>
+            </nav>
+            <div class="header-section">
+                <h2>Find Your Perfect Stay</h2>
+                <div class="search-bar">
+                <form action="rooms.html" method="GET" class="row g-3">
+                    <div class="col-md-6">
+                        <label for="stayType" class="form-label">Stay Type</label>
+                        <select id="stayType" name="stayType" class="form-select">
+                            <option selected>Dormitory</option>
+                            <option>Apartment</option>
+                        </select>
+                    </div>
+                    <div class="col-md-3">
+                        <label for="checkIn" class="form-label">Check-In</label>
+                        <input type="date" id="checkIn" name="checkIn" class="form-control"/>
+                    </div>
+                    <div class="col-md-3">
+                        <label for="checkOut" class="form-label">Check-Out</label>
+                        <input type="date" id="checkOut" name="checkOut" class="form-control"/>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="guests" class="form-label">Guests</label>
+                        <select id="guests" name="guests" class="form-select">
+                            <option selected>1 Room, 2 Adults</option>
+                        </select>
+                    </div>
+                    <div class="col-md-6 d-flex justify-content-center align-items-center">
+                        <button type="submit" class="btn btn-primary w-100">Search</button>
+                    </div>
+                </form> 
+                </div>
+            </div>
+            <div class="home-stay">
+                <img src={room1} alt=""/>
+                <img src={room2} alt=""/>
+                <img src={room3} alt=""/>
+            </div>
         </div>
     );
 }
